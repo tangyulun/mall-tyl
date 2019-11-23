c35737f5738519987f39cd500aade3a89aa49773
tangyulun <17673256238@163.com>
2019-11-23 13:07:00 +0800

    1

--- src/main/java/com/springboot/mall/controller/MallUserController.java
+++ src/main/java/com/springboot/mall/controller/MallUserController.java
@@ -2,6 +2,8 @@ package com.springboot.mall.controller;
 
 import com.springboot.mall.dao.MallUserMapper;
 import com.springboot.mall.model.MallUser;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+/**
+ * MallUserController
+ */
+@Api(tags = "MallUserController", description = "测试")
 @RequestMapping("/mall")
 @RestController
 public class MallUserController {
     @Autowired
     private MallUserMapper mallUserMapper;
+
+    @ApiOperation("根据ID查询")
     @GetMapping("/id")
     public MallUser getUser(@RequestParam("id") BigDecimal id) {
         MallUser mallUser = mallUserMapper.selectByPrimaryKey(id);
