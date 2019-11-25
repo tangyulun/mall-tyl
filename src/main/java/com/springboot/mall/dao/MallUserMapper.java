93a2a2ef933b90d55938c88e6175f989079dc1ce
tangyulun <17673256238@163.com>
2019-11-25 19:42:11 +0800

    1

--- src/main/java/com/springboot/mall/dao/MallUserMapper.java
+++ src/main/java/com/springboot/mall/dao/MallUserMapper.java
@@ -7,37 +7,37 @@ import java.math.BigDecimal;
 public interface MallUserMapper {
     /**
      *
-     * @mbggenerated 2019-11-20
+     * @mbggenerated 2019-11-24
      */
     int deleteByPrimaryKey(BigDecimal id);
 
     /**
      *
-     * @mbggenerated 2019-11-20
+     * @mbggenerated 2019-11-24
      */
     int insert(MallUser record);
 
     /**
      *
-     * @mbggenerated 2019-11-20
+     * @mbggenerated 2019-11-24
      */
     int insertSelective(MallUser record);
 
     /**
      *
-     * @mbggenerated 2019-11-20
+     * @mbggenerated 2019-11-24
      */
     MallUser selectByPrimaryKey(BigDecimal id);
 
     /**
      *
-     * @mbggenerated 2019-11-20
+     * @mbggenerated 2019-11-24
      */
     int updateByPrimaryKeySelective(MallUser record);
 
     /**
      *
-     * @mbggenerated 2019-11-20
+     * @mbggenerated 2019-11-24
      */
     int updateByPrimaryKey(MallUser record);
 }
